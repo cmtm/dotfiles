b8f77bedc56a2a326b0eb4185387818183ade2d0
Chris Morin <chris@lightsource.ai>
2024-10-07 22:05:36 -0700

    add wezterm

---
+++ wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+config.color_scheme = 'Selenized Dark (Gogh)'
+config.default_prog = { '/usr/bin/nu' }
+config.hide_tab_bar_if_only_one_tab = true
+config.scrollback_lines = 100000
+
+return config
