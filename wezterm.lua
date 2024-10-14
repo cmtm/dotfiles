4cda38a8f2961d3d7ee1e86052646193b12a09cf
Chris Morin <chris@lightsource.ai>
2024-10-14 11:19:24 -0700

    misc config

--- wezterm.lua
+++ wezterm.lua
@@ -6,5 +6,6 @@ config.color_scheme = 'Selenized Dark (Gogh)'
 config.default_prog = { '/usr/bin/nu' }
 config.hide_tab_bar_if_only_one_tab = true
 config.scrollback_lines = 100000
+config.enable_kitty_keyboard = true
 
 return config
